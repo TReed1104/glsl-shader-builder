ac4bfec2a937d175cca0322b220acd837cf87b4a
Tom Reed <t.reed1104@gmail.com>
2019-04-05 11:28:54 +0100

    Implementred the error checking for if the file is not found or not openable

--- glsl-shader-builder.py
+++ glsl-shader-builder.py
@@ -8,11 +8,15 @@ def main():
     # Get the shader main file name
     shaderName = sys.argv[1]
     print("Shader Name:", shaderName)
-    shaderFile = open(shaderName, "r+")    # Open the file
-    for x in shaderFile:
-        print(x)
-    shaderFile.close()  # close the file
-    print()
+
+    try:
+        shaderFile = open(shaderName, "r+")    # Open the file
+        for x in shaderFile:
+            print(x)
+        shaderFile.close()  # close the file
+        print()
+    except (OSError, IOError) as e:
+        print("An error occured: ", e)
 
 if __name__ == '__main__':
     main()
