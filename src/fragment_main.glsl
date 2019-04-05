e5d7efea5653f7b27e949f98e8266c60b167764b
Tom Reed <t.reed1104@gmail.com>
2019-04-05 14:59:48 +0100

    amended the #includes file locations to be in the source folder

--- src/fragment_main.glsl
+++ src/fragment_main.glsl
@@ -3,9 +3,9 @@ in vec3 fragmentColour;
 
 out vec4 outputColour;
 
-#include global_uniforms.glsl
+#include src/global_uniforms.glsl
 
-#include global_functions.glsl
+#include src/global_functions.glsl
 
 void main() {
 	outputColour = vec4(fragmentColour, 1.0f);
