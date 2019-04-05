c8e8e90515ab39d5c8e06129e5ca77e42c10de0b
Tom Reed <t.reed1104@gmail.com>
2019-04-06 00:52:43 +0100

    trimmed the shadertoy globals to what I currently want to use

--- src/global_uniforms.glsl
+++ src/global_uniforms.glsl
@@ -1,11 +1,4 @@
 // Universal uniforms, these match shadertoys
 uniform vec3 iResolution;
 uniform float iTime;
-uniform float iTimeDelta;
-uniform float iFrame;
-uniform float iChannelTime[4];
 uniform vec4 iMouse;
-uniform vec4 iDate;
-uniform float iSampleRate;
-uniform vec3 iChannelResolution[4];
-uniform samplerXX iChanneli;
