0fa3d212bbb941d488151e3c05d76f6a7aec9eab
Tom Reed <t.reed1104@gmail.com>
2019-04-05 15:00:38 +0100

    Implemented the executeIncludes function, this goes through and replaces each of the #includes with the files text they link to

--- glsl-shader-builder.py
+++ glsl-shader-builder.py
@@ -1,4 +1,5 @@
 import sys
+import re
 
 def readFile(shaderName):
     fileContent = ""
@@ -13,14 +14,33 @@ def readFile(shaderName):
         print(">> ERROR! - An error occured: ", e)
         return fileContent
 
+def executeIncludes(fileText):
+    includeToken = "#include "
+    linesOfText = re.split("(\n)", fileText)
+    for index, line in enumerate(linesOfText):
+        indexTokenStartsAt = line.find(includeToken)
+        if indexTokenStartsAt != -1:
+            # get the file to includes name using the index of the token, length of token and length of line.
+            nameOfFileToInclude = line[(indexTokenStartsAt + len(includeToken)) : len(line)]
+            nameOfFileToInclude = nameOfFileToInclude.strip()          # Remove the leading or trailing spaces
+            print(">> #include found", nameOfFileToInclude)
+
+            fileTextToInclude = readFile(nameOfFileToInclude)
+            linesOfText[index] = fileTextToInclude
+    outputText = "".join(linesOfText)
+    return outputText
+    
+
 def main():
     print("---------------------------")
     print("Ivaldi, The Shader Smith")
     print("---------------------------")
     
-    shaderName = sys.argv[1]                    # Get the shader main file name
-    mainFileText = readFile(shaderName)          # Read the file
-    print("\n", mainFileText)
+    #shaderName = sys.argv[1]                    # Get the shader main file name
+    shaderName = "src/fragment_main.glsl"
+    mainFileText = readFile(shaderName)         # Read the file
+    builtShader = executeIncludes(mainFileText)
+    print(builtShader)
 
 if __name__ == '__main__':
     main()
