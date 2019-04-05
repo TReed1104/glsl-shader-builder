86b1ce0d47aa89591e7f734678eef5c3b4b1b219
Tom Reed <t.reed1104@gmail.com>
2019-04-05 10:31:35 +0100

    Implemented a test for getting the arguments passed to the script

--- glsl-shader-builder.py
+++ glsl-shader-builder.py
@@ -1,5 +1,13 @@
+import sys
+
 def main():
-    print("Hello World!")
+    print("-----------------------")
+    print("Starting: ", sys.argv[0])
+    print("-----------------------")
+    for args in sys.argv:
+        print(args)
+
+    print("")
 
 if __name__ == '__main__':
     main()
