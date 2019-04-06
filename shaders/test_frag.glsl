c77ba05b94fa9e6ad198313bd40b6177bea9aed9
Tom Reed <t.reed1104@gmail.com>
2019-04-06 22:39:35 +0100

    fixed the directory value passed to the #include directives

--- shaders/test_frag.glsl
+++ shaders/test_frag.glsl
@@ -3,9 +3,9 @@ in vec3 fragmentColour;
 
 out vec4 outputColour;
 
-#include src/global_fragment_uniforms.glsl
+#include shaders/global_fragment_uniforms.glsl
 
-#include src/global_fragment_functions.glsl
+#include shaders/global_fragment_functions.glsl
 
 void main() {
 	outputColour = vec4(fragmentColour, 1.0f);
