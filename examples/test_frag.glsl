7c8ce5d1bf3a57ca21ba957fea44db701faeaf05
Tom Reed <t.reed1104@gmail.com>
2019-04-07 00:02:09 +0100

    moved all the shader test files to an 'examples' folder

--- examples/test_frag.glsl
+++ examples/test_frag.glsl
@@ -3,9 +3,9 @@ in vec3 fragmentColour;
 
 out vec4 outputColour;
 
-#include shaders/global_fragment_uniforms.glsl
+#include examples/global_fragment_uniforms.glsl
 
-#include shaders/global_fragment_functions.glsl
+#include examples/global_fragment_functions.glsl
 
 void main() {
 	outputColour = vec4(fragmentColour, 1.0f);
