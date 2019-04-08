a1899eea5488b35138afafbc1998485a1a241c37
Tom Reed <t.reed1104@gmail.com>
2019-04-08 10:25:51 +0100

    added a fragment shader for using texture arrays

---
+++ shaders/texture_array_frag.glsl
@@ -0,0 +1,20 @@
+#version 330
+in vec3 fragmentColour;
+in vec2 UV;
+
+out vec4 outputColour;
+
+#include shaders/global_fragment_uniforms.glsl
+
+#include shaders/texture_uniforms.glsl
+uniform int u_textureArrayLayer;
+
+void main() {
+	if (u_hasTexture) {
+		outputColour = texture(u_textureSampler, vec3(UV, u_textureArrayLayer));
+	}
+	else {
+		// Texturing has not been setup, use the colour buffer.
+		outputColour = vec4(fragmentColour, 1.0f);
+	}
+}
