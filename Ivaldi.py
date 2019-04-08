6b58952b8edb6a4868b793213ed5a7d1e0eacc81
Tom Reed <t.reed1104@gmail.com>
2019-04-08 14:41:17 +0100

    Implemented reusing the source name for the compiled output

--- Ivaldi.py
+++ Ivaldi.py
@@ -5,7 +5,7 @@ import os
 def readFileToString(shaderName):
     fileContent = ""
     try:
-        shaderFile = open(shaderName, "r")    # Open the file to read
+        shaderFile = open(shaderName, "r")  # Open the file to read
         for x in shaderFile:
             fileContent += x
         shaderFile.close()  # close the file
@@ -29,15 +29,13 @@ def executeIncludes(fileText):
         indexTokenStartsAt = line.find(includeToken)
         if indexTokenStartsAt != -1:
             # get the file to includes name using the index of the token, length of token and length of line.
-            nameOfFileToInclude = line[(indexTokenStartsAt + len(includeToken)) : len(line)]
-            nameOfFileToInclude = nameOfFileToInclude.strip()          # Remove the leading or trailing spaces
+            nameOfFileToInclude = line[(indexTokenStartsAt + len(includeToken)):len(line)]
+            nameOfFileToInclude = nameOfFileToInclude.strip()  # Remove the leading or trailing spaces
             print(">>>>", includeToken, "found replacing with contents of file:", nameOfFileToInclude)
-
             fileTextToInclude = readFileToString(nameOfFileToInclude)
             linesOfText[index] = fileTextToInclude
     outputText = "".join(linesOfText)
     return outputText
-    
 
 def main():
     print("---------------------------\nIvaldi, The Shader Smith\n---------------------------")
@@ -46,27 +44,27 @@ def main():
         os.makedirs("output")
     except FileExistsError:
         pass
-    
-    if len(sys.argv) == 3:
+    numberOfParamtersSupplied = len(sys.argv)
+    if numberOfParamtersSupplied > 1 and numberOfParamtersSupplied <= 3:
         print(">> Building shader:", sys.argv[1])
-        mainShaderSource = readFileToString(sys.argv[1])        # Read the shader main
+        mainShaderSource = readFileToString(sys.argv[1])  # Read the shader main
         if mainShaderSource != "":
-            builtShaderSource = executeIncludes(mainShaderSource)           # Compile the shader file, compiling down the includes/requires
-            writeStringToFile("output/" + sys.argv[2], builtShaderSource)   # Write the compiled shader string to our target file
-            print(">> Shader source saved to:", "output/" + sys.argv[2])
+            builtShaderSource = executeIncludes(mainShaderSource)  # Compile the shader file, compiling down the includes/requires
+            if (numberOfParamtersSupplied == 3):
+                writeStringToFile("output/" + sys.argv[2], builtShaderSource)  # Write the compiled shader string to our target file
+                print(">> Shader source saved to:", "output/" + sys.argv[2])
+            else:
+                # TODO: trim the folder location from the name
+                fileName = sys.argv[1].split("\\")[-1]
+                writeStringToFile("output/" + fileName, builtShaderSource)  # Write the compiled shader string to our target file
+                print(">> Shader source saved to:", "output/" + fileName)
         else:
             print(">>>> ERROR! - unable to open main source file, please check the entered name")
-
     else:
-        print(">> ERROR!")
-        if len(sys.argv) == 1:
-            print(">>>> Please enter main source file name and target file name")
-        elif len(sys.argv) == 2:
-            print(">>>> Please enter target file name")
-        elif len(sys.argv) > 3:
-            print(">>>> Too many parameters supplied")
+        if numberOfParamtersSupplied > 3:
+            print(">> ERROR! - Too many command line parameters supplied")
         else:
-            print(">>>> Unknown error - please contact developer")
+            print(">> ERROR! - Please check your supplied parameters")
 
 if __name__ == '__main__':
     main()
