ec2e4adc9baa01c61a02b6f4c248d8242463ce06
Tom Reed <t.reed1104@gmail.com>
2019-04-09 11:58:16 +0100

    removed the examples folder due to it being defunct

--- examples/fragment_in.glsl
+++
@@ -1,3 +0,0 @@
-// Generic In variables for a fragment shader (mesh colour and TexCoords)
-in vec3 fragmentColour;
-in vec2 UV;
--- examples/global_functions.glsl
+++
@@ -1,5 +0,0 @@
-float random(vec2 p) {
-	// Pseudo random number generator from seed
-	const vec2 r = vec2(23.1406926327792690, 2.6651441426902251);// e^pi (Gelfond's constant) & 2^sqrt(2) (Gelfond-Schneider constant)
-	return fract(cos(mod(123456789.0f, 1e-7+256.0f * dot(p, r))));
-}
