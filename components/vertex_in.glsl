a9b905f21eb67456af05039cecd308b0b3f08cd9
Tom Reed <t.reed1104@gmail.com>
2019-04-09 11:09:57 +0100

    commented vertex_in.glsl

--- components/vertex_in.glsl
+++ components/vertex_in.glsl
@@ -1,3 +1,4 @@
+// Generic In variables for a vertex shader, these are normally values from an object mesh
 layout (location = 0) in vec3 vertexPosition;
 layout (location = 1) in vec3 vertexColor;
 layout (location = 2) in vec2 vertexUV;
