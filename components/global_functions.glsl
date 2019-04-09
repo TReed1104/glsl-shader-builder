780d6ca0e976566c9bf1ac90eaac304d43c6680a
Tom Reed <t.reed1104@gmail.com>
2019-04-09 11:21:17 +0100

    added a newline in the global_functions component, for my own sake

--- components/global_functions.glsl
+++ components/global_functions.glsl
@@ -3,6 +3,7 @@ float random(vec2 p) {
 	const vec2 r = vec2(23.1406926327792690, 2.6651441426902251);// e^pi (Gelfond's constant) & 2^sqrt(2) (Gelfond-Schneider constant)
 	return fract(cos(mod(123456789.0f, 1e-7+256.0f * dot(p, r))));
 }
+
 float noise(in vec2 st){
 	// taken from examples online
 	vec2 i = floor(st);
